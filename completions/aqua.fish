7e51591a11db28c30a0f30521de1ef9c7fcac8bc
grimoh <dev@grimoh.net>
2022-11-16 00:23:29 +0900

    Compatible with Aqua 1.24.0 version

--- completions/aqua.fish
+++ completions/aqua.fish
@@ -17,7 +17,7 @@ function __fish_aqua_using_command
 end
 
 function __fish_aqua_help_topics
-    for c in init install generate which exec list generate-registry completion version cp update-checksum
+    for c in init init-policy install update-aqua generate which exec list generate-registry completion version cp update-checksum
         printf "%s\thelp topic\n" $c
     end
 end
@@ -27,14 +27,21 @@ complete -f -c aqua -n "__fish_aqua_using_command help" -a "(__fish_aqua_help_to
 
 complete -f -c aqua -n "__fish_aqua_needs_command" -a init -d "Create a configuration file if it doesn't exist"
 
+complete -f -c aqua -n "__fish_aqua_needs_command" -a init-policy -d "Create a policy file if it doesn't exist"
+
 complete -f -c aqua -n "__fish_aqua_needs_command" -a install -d "Install tools"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -l only-link -s l -d "create links but skip downloading packages (default: false)"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -l test -d "test file.src after installing the package (default: false)"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -l all -s a -d "install all aqua configuration packages (default: false)"
+complete -f -c aqua -n "__fish_aqua_needs_command install" -l tags -s t -d "filter installed packages with tags"
+complete -f -c aqua -n "__fish_aqua_needs_command install" -l exclude-tags -d "exclude installed packages with tags"
+
+complete -f -c aqua -n "__fish_aqua_needs_command" -a update-aqua -d "Update aqua"
 
 complete -f -c aqua -n "__fish_aqua_needs_command" -a generate -d "Search packages in registries and output the configuration interactively"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -s f -d "the file path of packages list. When the value is "-", the list is passed from the standard input"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -s i -d "Insert packages to configuration file (default: false)"
+complete -f -c aqua -n "__fish_aqua_needs_command install" -l pin -d "Pin version (default: false)"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -s o -d "inserted file"
 complete -f -c aqua -n "__fish_aqua_needs_command install" -l select-version -s s -d "Select the installed version interactively (default: false)"
 
@@ -53,6 +60,8 @@ complete -f -c aqua -n "__fish_aqua_needs_command" -a version -d "Show version"
 complete -f -c aqua -n "__fish_aqua_needs_command" -a cp -d "Copy executable files in a directory"
 complete -f -c aqua -n "__fish_aqua_needs_command cp" -s o -d 'destination directory (default: "dist")'
 complete -f -c aqua -n "__fish_aqua_needs_command cp" -l all -s a -d "install all aqua configuration packages (default: false)"
+complete -f -c aqua -n "__fish_aqua_needs_command cp" -l tags -s t -d "filter installed packages with tags"
+complete -f -c aqua -n "__fish_aqua_needs_command cp" -l exclude-tags -d "exclude installed packages with tags"
 
 complete -f -c aqua -n "__fish_aqua_needs_command" -a update-checksum -d "Create or Update .aqua-checksums.json"
 complete -f -c aqua -n "__fish_aqua_needs_command cp" -l all -s a -d "Create or Update all .aqua-checksums.json including global configuration (default: false)"
